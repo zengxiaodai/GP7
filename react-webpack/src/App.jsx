588aa45a831b0ac1cf749320a57aaf9df5c0dce6
geekxia <448914712@qq.com>
2021-11-19 11:50:53 +0800

    day25 am

--- react-webpack/src/App.jsx
+++ react-webpack/src/App.jsx
@@ -8,16 +8,16 @@ import React from 'react'
 // import TestCondition from '@/views/study/12/TestCondition'
 // import TestList from '@/views/study/12/TestList'
 // import TestForm from '@/views/study/12/TestForm'
-import TestLife from '@/views/study/12/TestLife'
+// import TestLife from '@/views/study/12/TestLife'
 
-
-
+// import TestLift from '@/views/study/14/TestLift'
+import TestCombine from '@/views/study/14/TestCombine'
 
 // 函数式组件（App根组件）
 export default () => {
   return (
     <div className='app'>
-      <TestLife />
+      <TestCombine />
     </div>
   )
 }
